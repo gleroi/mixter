f9335b06ebad5002a81d9c1190001a2c7eb72937
fpellet <git@florentpellet.com>
2018-02-02 21:37:51 +0100

    [Core][Timeline][Test OK] Given a message saved for several users When remove this message Then remove this message of all users

--- Mixter/Infrastructure.Core.fs
+++ Mixter/Infrastructure.Core.fs
@@ -16,3 +16,6 @@ type MemoryTimelineMessageStore() =
     [<Query>]
     member __.GetMessagesOfUser userId =
         store |> Seq.filter (fun p -> p.Owner = userId)
+
+    member __.Delete messageId =
+        store.RemoveWhere(fun p -> p.MessageId = messageId) |> ignore
