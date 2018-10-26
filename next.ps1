9824bfa5f5bf810418b326216fdf82227126a878
Guillaume Leroi <guillaume-externe.leroi@enedis.fr>
2018-10-26 16:20:17 +0200

    Add test navigation commands

--- next.ps1
+++ next.ps1
@@ -1,10 +1,10 @@
 git add -A 
 git commit -m "Resolve test"
-git merge step3-test2
+git merge step3-test3
 Write-Host ""
 Write-Host ""
 Write-Host -f green "========================"
-Write-Host -f green "===  STEP 3 - Test 2 ==="
+Write-Host -f green "===  STEP 3 - Test 3 ==="
 Write-Host -f green "========================"
 Write-Host ""
 Write-Host ""
