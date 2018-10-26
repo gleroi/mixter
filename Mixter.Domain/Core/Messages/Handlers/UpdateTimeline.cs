e657bbeb3f47909ad25c14243f801947cb30170c
Guillaume Leroi <guillaume-externe.leroi@enedis.fr>
2018-10-26 17:24:26 +0200

    Resolve test

--- Mixter.Domain/Core/Messages/Handlers/UpdateTimeline.cs
+++ Mixter.Domain/Core/Messages/Handlers/UpdateTimeline.cs
@@ -1,7 +1,21 @@
-﻿namespace Mixter.Domain.Core.Messages.Handlers
+﻿using Mixter.Domain.Core.Messages.Events;
+
+namespace Mixter.Domain.Core.Messages.Handlers
 {
     [Handler]
-    public class UpdateTimeline
+    public class UpdateTimeline : IEventHandler<MessageQuacked>
     {
+        private readonly ITimelineMessageRepository repository;
+
+        public UpdateTimeline(ITimelineMessageRepository repository) {
+            this.repository = repository;
+        }
+
+        public void Handle(MessageQuacked evt)
+        {
+            var tlMsg = new TimelineMessageProjection(evt.Author, evt.Author, evt.Content, evt.Id);
+            repository.Save(tlMsg);
+        }
+
     }
 }
