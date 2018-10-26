c59a78508bc702b255cbac402d1c075fda3f3459
Guillaume Leroi <guillaume-externe.leroi@enedis.fr>
2018-10-26 16:19:56 +0200

    Add step navigation commands

---
+++ jumpToNextStep.ps1
@@ -0,0 +1,12 @@
+git add -A
+git commit -m "Abort test"
+git checkout -b workshop-step2 step2 
+git merge step2-test1 *> $null
+git checkout --ours . 
+git add . 
+git commit -m "Merge with test branch" 
+Write-Host ""
+Write-Host ""
+Get-Content stepsDoc/step2.txt | Write-Host -f green
+Write-Host ""
+Write-Host ""
