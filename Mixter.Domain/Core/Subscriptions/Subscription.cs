aaf220b6c5803946ddfb53923e72e68b8312c35d
Guillaume Leroi <guillaume-externe.leroi@enedis.fr>
2018-10-26 17:34:46 +0200

    Resolve test

--- Mixter.Domain/Core/Subscriptions/Subscription.cs
+++ Mixter.Domain/Core/Subscriptions/Subscription.cs
@@ -1,7 +1,14 @@
-﻿namespace Mixter.Domain.Core.Subscriptions
+﻿using Mixter.Domain.Core.Subscriptions.Events;
+using Mixter.Domain.Identity;
+
+namespace Mixter.Domain.Core.Subscriptions
 {
     [Aggregate]
     public class Subscription
     {
+        public static void FollowUser(IEventPublisher eventPublisher, UserId follower, UserId followee) {
+            var evt = new UserFollowed(new SubscriptionId(follower, followee));
+            eventPublisher.Publish(evt);
+        }
     }
 }
