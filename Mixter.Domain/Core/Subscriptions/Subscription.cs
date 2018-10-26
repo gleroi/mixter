72b03e377e5c52a1a3fea574ac56308c15d24c29
Guillaume Leroi <guillaume-externe.leroi@enedis.fr>
2018-10-26 17:44:13 +0200

    Resolve test

--- Mixter.Domain/Core/Subscriptions/Subscription.cs
+++ Mixter.Domain/Core/Subscriptions/Subscription.cs
@@ -6,9 +6,36 @@ namespace Mixter.Domain.Core.Subscriptions
     [Aggregate]
     public class Subscription
     {
+        private DecisionProjection _projection = new DecisionProjection();
+
         public static void FollowUser(IEventPublisher eventPublisher, UserId follower, UserId followee) {
             var evt = new UserFollowed(new SubscriptionId(follower, followee));
             eventPublisher.Publish(evt);
+        }
+
+        public Subscription(IDomainEvent[] events) {
+            foreach (var @event in events)
+            {
+                _projection.Apply(@event);
+            }
+        }
+
+        public void Unfollow(IEventPublisher eventPublisher) {
+            var evt = new UserUnfollowed(_projection.Id);
+            eventPublisher.Publish(evt);
+        }
+
+        private class DecisionProjection : DecisionProjectionBase
+        {
+            public DecisionProjection() {
+                AddHandler<UserFollowed>(When);
+            }
+
+            public SubscriptionId Id { get; private set; }
+
+            void When(UserFollowed evt) {
+                this.Id = evt.SubscriptionId;
+            }
         }
     }
 }
